ee02822b3faa59a98ffe8a928e1d10fbf3ddfc7d
Lucas <lucasmercado101@gmail.com>
2020-09-03 15:41:04 -0300

    removed the console.log

--- src/js/index.js
+++ src/js/index.js
@@ -2,7 +2,6 @@ const socket = io.connect("/");
 
 socket.on("update rooms", (roomsData) => {
   $(".card p").each(function (i) {
-    console.log(roomsData, i);
     $(this).empty().append(`${roomsData[i]} <br> Players`);
   });
 });
